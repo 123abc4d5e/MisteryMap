392d38b46d619e61f2ec1fb89bc74b6bf492d692
estraviado <markelollo97@gmail.com>
2017-05-26 01:49:40 +0200

    Rejected commit: Default

--- app/src/main/java/com/marluki/misterymap/MainActivity.java
+++ app/src/main/java/com/marluki/misterymap/MainActivity.java
@@ -225,13 +225,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_alien) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_fantasma) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_sin_resolver) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_historico) {
 
         } else if (id == R.id.nav_share) {
 
